1cdbe1a5591f51e734ecbf1bf3f090775cf51a74
laziv <ilazerka4678@bths.edu>
2022-09-21 16:40:51 -0400

    added favorite food

--- src/print.java
+++ src/print.java
@@ -1,8 +1,9 @@
-public class main {
+public class print {
 
     public static void main(String[] args) {
 
         System.out.println("Ivan Lazerka");
         System.out.println("My favorite movie is Dead Man's Bluff");
+        System.out.println("My favorite food is pizza");
     }
 }
